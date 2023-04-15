ac68643664a4f95ec0b6894a0ac015c2be7feaf0
Izabela Świadkowska <izaswiadkowska@gmail.com>
2023-04-15 11:15:53 +0200

    task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+function countCategoriesAndFindHeaderAndCount() {
+  const listItems = document.querySelectorAll('ul#categories li.item');
+  const numCategories = listItems.length;
+  console.log('Number of categories: ${numCategories}');
+  listItems.forEach((item) => {
+    const header = item.querySelector('h2').innerText;
+    const count = item.querySelectorAll('li').length;
+    console.log('Category: ${header}');
+    console.log('Elements:${count}');
+  });
+}
+countCategoriesAndFindHeaderAndCount('listItems');
